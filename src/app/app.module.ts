221ecda03bf541e3cf0d6b604bd61fad5add19aa
Jade Hollowell <jade.hollowell@66degrees.com>
2023-04-13 17:40:19 -0700

    commiting to test affected

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
+import { SharedUiModule } from '@store/shared/ui';
+
 import { AppComponent } from './app.component';
-import { appRoutes } from './app.routes';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { ShopComponent } from './shop/shop.component';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
+  declarations: [AppComponent, NxWelcomeComponent, ShopComponent],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
+    SharedUiModule,
+    RouterModule.forRoot([
+      {
+        path: 'cart',
+        loadChildren: () => import('@store/cart').then((m) => m.CartModule),
+      },
+      {
+        path: '**',
+        component: ShopComponent,
+      },
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent],
